75c54b55aba8ac14079ff56ff630753c6676dff7
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-11-25 22:32:32 +0530

    having a bug in array of object

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -7,7 +7,11 @@ import firebase from 'firebase/app'
 import React from 'react'
 
 function Main() {
-    const [input,setinput]=useState("")
+    const [todo, settodo] = useState({
+        todoname: "",
+        tododate: "",
+        todotime: ""
+      }); 
     
     const [todos, settodos] = useState([]);
     // when the app loads we need to listen the db and fetch new todo when added/deleted with the help of useeffect
@@ -16,26 +20,42 @@ function Main() {
       // this code runs when app loads
       db.collection("todos").orderBy('timestamp','desc').onSnapshot(snapshot=>{
         // console.log(snapshot.docs.map(doc=>doc))
-        settodos(snapshot.docs.map(doc=>({id:doc.id,todo:doc.data().todo})))
+        settodos(snapshot.docs.map(doc=>({id:doc.id,todo:doc.data()})))
       })
     }, [])
     
     const addtodos=(e)=>{
       e.preventDefault();
       db.collection("todos").add({
-        todo:input,
+        todo:todos,
         timestamp:firebase.firestore.FieldValue.serverTimestamp()
       })
       
-      settodos([...todos,input])
-      setinput("")//clear input field after clicking the button
+      settodos([...todos,todo])
+      settodo("")//clear input field after clicking the button
     }
     return (
         <div>
             <form action="">
-      <input type="text" id="inp" value={input} onChange={(e)=>setinput(e.target.value)}/>
+            <input
+        type="text"
+        value={todo.todoname}
+        onChange={(e) => settodo({ ...todo, todoname: e.target.value })}
+      />
+      <input
+        type="date"
+        onChange={(e) => {
+          settodo({ ...todo, tododate: e.target.value.split("T")[0] });
+        }}
+      />
+      <input
+        type="time"
+        onChange={(e) => {
+          settodo({ ...todo, todotime: e.target.value.split("T")[0] });
+        }}
+      />
      
-      <button id="btn" type="submit" disabled={!input} onClick={addtodos}>add</button>
+      <button id="btn" type="submit" disabled={!todo} onClick={addtodos}>add</button>
       </form>
       <ul>
         {
